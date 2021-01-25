5302ac29f9cf860a1482a93446f9277e77e8171a
biswa7064 <biswaranjan16856@gmail.com>
2021-01-25 22:09:49 +0530

    userInput customhook created

--- src/customHook/useInput.jsx
+++ src/customHook/useInput.jsx
@@ -3,9 +3,20 @@ import React, { useState } from 'react'
 
 const useInput = (initialvalue = '') => {
     const [value,setValue] = useState(initialvalue)
-    return (
-        
-    )
-}
+        const reSet = ()=>{
+            setValue(initialvalue)
+            
+        }
+        const bind ={
+             value,
+             onChange: (e)=>{
+                 setValue(e.target.value)
+             }   
+        }
+
+        return [value,reSet,bind]
+
+    }
+
 
 export default useInput
