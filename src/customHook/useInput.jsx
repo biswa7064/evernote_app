2d37cff6f12bba9179c2e27c7d55f47c04b696ea
biswa7064 <biswaranjan16856@gmail.com>
2021-01-25 21:47:52 +0530

    initialize app and also finished navbar using with react router dom

---
+++ src/customHook/useInput.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+
+const useInput = (initialvalue = '') => {
+    const [value,setValue] = useState(initialvalue)
+    return (
+        
+    )
+}
+
+export default useInput
