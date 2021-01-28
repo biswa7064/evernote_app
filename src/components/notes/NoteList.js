5c60d10a286e86d336fa766ce205315719ceda9d
biswa7064 <biswaranjan16856@gmail.com>
2021-01-28 13:03:23 +0530

    Create the noteslist

--- src/components/notes/NoteList.js
+++ src/components/notes/NoteList.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Note from './Note'
 
-const NoteList = () => {
+const NoteList = ({notes}) => {
     return (
-        <div>
-            NoteList
+        <div className='notesList'>
+            {notes && notes.map(note=><Note note={note} key={note.id}/>)}
         </div>
     )
 }
