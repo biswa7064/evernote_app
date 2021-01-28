bc264fbe7619f36e55fa36d31364603266364a66
biswa7064 <biswaranjan16856@gmail.com>
2021-01-28 13:27:35 +0530

    Refine the note component

--- src/components/notes/Note.js
+++ src/components/notes/Note.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Note = ({note}) => {
     return (
-        <div>
-           {note.title} 
+        <div className='note blue'>
+            <div className='right-align'>
+                <i className='material-icons red-text' style={{cursor:'pointer'}}>favorite</i>
+                <i className='material-icons' style={{cursor:'pointer'}}>delete</i>
+            </div> 
+            <h5 className='black-text'>{note.title}</h5>
+            <p className='truncate black-text'>{note.content}</p>
+            <p className='black-text'>day ago</p>
+            <div className='right-align'><i className="material-icons black-text">edit</i></div>
+
         </div>
     )
 }
