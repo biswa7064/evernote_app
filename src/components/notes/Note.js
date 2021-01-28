55c9f9372adcc00080a1bae0ed51bc7549079b27
biswa7064 <biswaranjan16856@gmail.com>
2021-01-28 13:34:22 +0530

    Create delete note action in Note.js with delete onClick

--- src/components/notes/Note.js
+++ src/components/notes/Note.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { deleteNote } from '../../store/actions/noteaction'
+
+
 
 const Note = ({note}) => {
+    const dispatch = useDispatch()
+
+    const deleteHandler = ()=>{
+        dispatch(deleteNote(note))
+    }
+
     return (
         <div className='note blue'>
             <div className='right-align'>
                 <i className='material-icons red-text' style={{cursor:'pointer'}}>favorite</i>
-                <i className='material-icons' style={{cursor:'pointer'}}>delete</i>
+                <i className='material-icons' style={{cursor:'pointer'}} onClick={deleteHandler}>delete</i>
             </div> 
             <h5 className='black-text'>{note.title}</h5>
             <p className='truncate black-text'>{note.content}</p>
